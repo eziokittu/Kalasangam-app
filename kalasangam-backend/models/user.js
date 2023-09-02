82724d4f564baab03ccefb4d61528ed426df3823
Bodhisatta Bhattacharjee <bodhisatta1999@gmail.com>
2023-09-02 21:32:46 +0530

    learning

--- kalasangam-backend/models/user.js
+++ kalasangam-backend/models/user.js
@@ -1,16 +1,14 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
-
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true, minlength: 6 },
-  image: { type: String, required: true },
-  places: [{ type: mongoose.Types.ObjectId, required: true, ref: 'Place' }]
+    name: {type: String, required: true },
+    email: {type: String, required: true, unique: true },
+    image: {type: String, required: true, minLength: 4 },
+    password: {type: String, required: true },
+    products: [{type: mongoose.Types.ObjectId, required: true, ref: 'Product' }]
 });
 
 userSchema.plugin(uniqueValidator);
-
 module.exports = mongoose.model('User', userSchema);
