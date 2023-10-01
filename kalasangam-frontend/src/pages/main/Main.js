c51f3051fc37c53002c37df485fba824c5ac1a17
Bodhisatta Bhattacharjee <bodhisatta1999@gmail.com>
2023-10-01 06:18:28 +0530

    so many errors, fked myBrain,sleep gone to fix thm

--- kalasangam-frontend/src/pages/main/Main.js
+++ kalasangam-frontend/src/pages/main/Main.js
@@ -1,56 +1,57 @@
-// import React from 'react'
-// import './Main.css';
-// import '../../reusable/reusable.css'; // Adjust the path based on your structure
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Main.css';
+import '../../reusable/reusable.css'; // Adjust the path based on your structure
 
-// export default function Main(props) {
-//   return (
-//     <>
-//       <main className="main">
-//         {/* <!--==================== HOME ====================--> */}
-//         <section className="home section" id="home">
-//            <img src={props.homeImage} alt="home image" className="home__bg"/>
-//            <div className="home__shadow"></div> 
+export default function Main(props) {
+  return (
+    <>
+      <main className="main">
+        {/* <!--==================== HOME ====================--> */}
+        <section className="home section" id="home">
+           <img src={props.homeImage} alt="home" className="home__bg"/>
+           <div className="home__shadow"></div> 
            
-//            <div className="home__container containter grid">
-//                <div className="home__data">
-//                    <h3 className="home__subtitle">
-//                        <i className="ri-leaf-fill"></i>KalaSangam
-//                    </h3>
-//                    <h1 className="home__title">
-//                        Explore<br/>
-//                        The World of Hand made Items
-//                    </h1>
-//                    <p className="home_description">
-//                        Discover a wide range of exqusite handicrafted items at KalaSangam's commerce store. Find the perfect piece to elevate your style.
-//                    </p>
+           <div className="home__container containter grid">
+               <div className="home__data">
+                   <h3 className="home__subtitle">
+                       <i className="ri-leaf-fill"></i>KalaSangam
+                   </h3>
+                   <h1 className="home__title">
+                       Explore<br/>
+                       The World of Hand made Items
+                   </h1>
+                   <p className="home_description">
+                       Discover a wide range of exqusite handicrafted items at KalaSangam's commerce store. Find the perfect piece to elevate your style.
+                   </p>
                    
-//                    <a href="#" className="button">
-//                        Happy Shopping <i className="ri-arrow-right-line"></i>
-//                    </a>
-//                </div>
-//             </div>
-//        </section>
+                   <Link to="/products" className="button">
+                    Happy Shopping <i className="ri-arrow-right-line"></i>
+                   </Link>
+               </div>
+            </div>
+       </section>
 
-//          {/* <!--==================== ABOUT ====================--> */}
-//          <section className="about section" id="about">
+         {/* <!--==================== ABOUT ====================--> */}
+         <section className="about section" id="about">
             
-//          </section>
+         </section>
 
-//          {/* <!--==================== PRODUCTS ====================--> */}
-//          <section className="popular section" id="popular">
+         {/* <!--==================== PRODUCTS ====================--> */}
+         <section className="popular section" id="popular">
             
-//          </section>
+         </section>
          
-//          {/* <!--==================== EXPLORE ====================--> */}
-//          <section className="explore section" id="explore">
+         {/* <!--==================== EXPLORE ====================--> */}
+         <section className="explore section" id="explore">
             
-//          </section>
+         </section>
          
-//          {/* <!--==================== JOIN ====================--> */}
-//          <section className="join section">
+         {/* <!--==================== JOIN ====================--> */}
+         <section className="join section">
             
-//          </section>
-//       </main>
-//     </>
-//   )
-// }
+         </section>
+      </main>
+    </>
+  )
+}
--- kalasangam-frontend/src/pages/myProducts/UpdateProduct.js
+++ kalasangam-frontend/src/pages/myProducts/UpdateProduct.js
@@ -1,136 +1,136 @@
-import React, { useEffect, useState, useContext } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+// import React, { useEffect, useState, useContext } from 'react';
+// import { useParams, useNavigate } from 'react-router-dom';
 
-import Input from '../../reusable/FormElements/Input';
-import Button from '../../reusable/FormElements/Button';
-import Card from '../../reusable/UIElements/Card';
-import LoadingSpinner from '../../reusable/UIElements/LoadingSpinner';
-import ErrorModal from '../../reusable/UIElements/ErrorModal';
-import {
-  VALIDATOR_REQUIRE,
-  VALIDATOR_MINLENGTH
-} from '../../reusable/util/validators';
-import { useForm } from '../../reusable/hooks/form-hook';
-import { useHttpClient } from '../../reusable/hooks/http-hook';
-import { AuthContext } from '../../reusable/context/auth-context';
-import './UpdateProduct.css';
+// import Input from '../../reusable/FormElements/Input';
+// import Button from '../../reusable/FormElements/Button';
+// import Card from '../../reusable/UIElements/Card';
+// import LoadingSpinner from '../../reusable/UIElements/LoadingSpinner';
+// import ErrorModal from '../../reusable/UIElements/ErrorModal';
+// import {
+//   VALIDATOR_REQUIRE,
+//   VALIDATOR_MINLENGTH
+// } from '../../reusable/util/validators';
+// import { useForm } from '../../reusable/hooks/form-hook';
+// import { useHttpClient } from '../../reusable/hooks/http-hook';
+// import { AuthContext } from '../../reusable/context/auth-context';
+// import './UpdateProduct.css';
 
-const Updateproduct = () => {
-  const auth = useContext(AuthContext);
-  const { isLoading, error, sendRequest, clearError } = useHttpClient();
-  const [loadedProduct, setLoadedProduct] = useState();
-  const productId = useParams().productId;
-  const navigate = useNavigate();
+// const Updateproduct = () => {
+//   const auth = useContext(AuthContext);
+//   const { isLoading, error, sendRequest, clearError } = useHttpClient();
+//   const [loadedProduct, setLoadedProduct] = useState();
+//   const productId = useParams().productId;
+//   const navigate = useNavigate();
 
-  const [formState, inputHandler, setFormData] = useForm(
-    {   
-      title: {
-        value: '',
-        isValid: false
-      },
-      description: {
-        value: '',
-        isValid: false
-      }
-    },
-    false
-  );
+//   const [formState, inputHandler, setFormData] = useForm(
+//     {   
+//       title: {
+//         value: '',
+//         isValid: false
+//       },
+//       description: {
+//         value: '',
+//         isValid: false
+//       }
+//     },
+//     false
+//   );
 
-  useEffect(() => {
-    const fetchProduct = async () => {
-      try {
-        const responseData = await sendRequest(
-          `http://localhost:5000/api/products/${productId}`
-        );
-        setLoadedProduct(responseData.product);
-        setFormData(
-          {
-            title: {
-              value: responseData.product.title,
-              isValid: true
-            },
-            description: {
-              value: responseData.product.description,
-              isValid: true
-            }
-          },
-          true
-        );
-      } catch (err) {}
-    };
-    fetchProduct();
-  }, [sendRequest, productId, setFormData]);
+//   useEffect(() => {
+//     const fetchProduct = async () => {
+//       try {
+//         const responseData = await sendRequest(
+//           `http://localhost:5000/api/products/${productId}`
+//         );
+//         setLoadedProduct(responseData.product);
+//         setFormData(
+//           {
+//             title: {
+//               value: responseData.product.title,
+//               isValid: true
+//             },
+//             description: {
+//               value: responseData.product.description,
+//               isValid: true
+//             }
+//           },
+//           true
+//         );
+//       } catch (err) {}
+//     };
+//     fetchProduct();
+//   }, [sendRequest, productId, setFormData]);
 
-  const productUpdateSubmitHandler = async event => {
-    event.preventDefault();
-    try {
-      await sendRequest(
-        `http://localhost:5000/api/products/${productId}`,
-        'PATCH',
-        JSON.stringify({
-          title: formState.inputs.title.value,
-          description: formState.inputs.description.value
-        }),
-        {
-          'Content-Type': 'application/json',
-          Authorization: 'Bearer ' + auth.token
-        }
-      );
-      navigate('/' + auth.userId + '/products');
-    } catch (err) {}
-  };
+//   const productUpdateSubmitHandler = async event => {
+//     event.preventDefault();
+//     try {
+//       await sendRequest(
+//         `http://localhost:5000/api/products/${productId}`,
+//         'PATCH',
+//         JSON.stringify({
+//           title: formState.inputs.title.value,
+//           description: formState.inputs.description.value
+//         }),
+//         {
+//           'Content-Type': 'application/json',
+//           Authorization: 'Bearer ' + auth.token
+//         }
+//       );
+//       navigate('/' + auth.userId + '/products');
+//     } catch (err) {}
+//   };
 
-  if (isLoading) {
-    return (
-      <div className="center">
-        <LoadingSpinner />
-      </div>
-    );
-  }
+//   if (isLoading) {
+//     return (
+//       <div className="center">
+//         <LoadingSpinner />
+//       </div>
+//     );
+//   }
 
-  if (!loadedProduct && !error) {
-    return (
-      <div className="center">
-        <Card>
-          <h2>Could not find product!</h2>
-        </Card>
-      </div>
-    );
-  }
+//   if (!loadedProduct && !error) {
+//     return (
+//       <div className="center">
+//         <Card>
+//           <h2>Could not find product!</h2>
+//         </Card>
+//       </div>
+//     );
+//   }
 
-  return (
-    <React.Fragment>
-      <ErrorModal error={error} onClear={clearError} />
-      {!isLoading && loadedProduct && (
-        <form className="product-form" onSubmit={productUpdateSubmitHandler}>
-          <Input
-            id="title"
-            element="input"
-            type="text"
-            label="Title"
-            validators={[VALIDATOR_REQUIRE()]}
-            errorText="Please enter a valid title."
-            onInput={inputHandler}
-            initialValue={loadedProduct.title}
-            initialValid={true}
-          />
-          <Input
-            id="description"
-            element="textarea"
-            label="Description"
-            validators={[VALIDATOR_MINLENGTH(5)]}
-            errorText="Please enter a valid description (min. 5 characters)."
-            onInput={inputHandler}
-            initialValue={loadedProduct.description}
-            initialValid={true}
-          />
-          <Button type="submit" disabled={!formState.isValid}>
-            UPDATE Product
-          </Button>
-        </form>
-      )}
-    </React.Fragment>
-  );
-};
+//   return (
+//     <React.Fragment>
+//       <ErrorModal error={error} onClear={clearError} />
+//       {!isLoading && loadedProduct && (
+//         <form className="product-form" onSubmit={productUpdateSubmitHandler}>
+//           <Input
+//             id="title"
+//             element="input"
+//             type="text"
+//             label="Title"
+//             validators={[VALIDATOR_REQUIRE()]}
+//             errorText="Please enter a valid title."
+//             onInput={inputHandler}
+//             initialValue={loadedProduct.title}
+//             initialValid={true}
+//           />
+//           <Input
+//             id="description"
+//             element="textarea"
+//             label="Description"
+//             validators={[VALIDATOR_MINLENGTH(5)]}
+//             errorText="Please enter a valid description (min. 5 characters)."
+//             onInput={inputHandler}
+//             initialValue={loadedProduct.description}
+//             initialValid={true}
+//           />
+//           <Button type="submit" disabled={!formState.isValid}>
+//             UPDATE Product
+//           </Button>
+//         </form>
+//       )}
+//     </React.Fragment>
+//   );
+// };
 
-export default Updateproduct;
+// export default Updateproduct;
